f82cb5b0574faf39bc3d9211191a5bda3fc12c30
MarinaBajic <marinabajic10@gmail.com>
2026-05-10 15:19:14 +0200

    example template

---
+++ vezbe5/hello-template/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Child } from './child/child';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Child],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App { }
